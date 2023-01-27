5d0c2564fb34616ede287b32dce9f6cc861a20f0
while0x1 <94356664+while0x1@users.noreply.github.com>
2023-01-28 00:06:31 +0800

    Update client.sh

--- client.sh
+++ client.sh
@@ -2,10 +2,17 @@
 
 AUTH_TOKEN="<yourAuthToken>"
 
-tipData=$(cardano-cli query tip --mainnet)
-block=$(jq '.block' <<< "$tipData")
+tipData=$(cardano-cli query tip --mainnet 2> errors.txt)
 
-curl -s -X POST -H "Content-Type: application/json" -H "Auth: $AUTH_TOKEN" -d '{
+if [ $? -ne 0 ]; then
+   echo "$tipData is an invalid command."
+else
+   echo "$cmd_name is a valid command."
+   block=$(jq '.block' <<< "$tipData")
+   curl -s -X POST -H "Content-Type: application/json" -H "Auth: $AUTH_TOKEN" -d '{
   "id": '"$AUTH_TOKEN"',
   "block": '"$block"'
 }' http://localhost:5000/metrics
+fi
+
+
